9be3e8f764cd79e1ac2bc41228f1091dbaab8b24
krutin47 <krutin47trivedi@gmail.com>
2021-06-08 02:08:31 -0300

    setting up the Initial project

---
+++ app.js
@@ -0,0 +1,35 @@
+/**
+ * @file Root file for the backend of the Application
+ * @author Team 2
+*/
+
+//importing the Components and required Modules
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// const mongoose = require('mongoose');
+
+var app = express();
+const port = process.env.PORT || 5000;
+
+//require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+// establishing mongo atlas connections
+// mongoose.connect('',
+//                   { useNewUrlParser: true, useCreateIndex: true}
+//                 );
+// const connection = mongoose.connection;
+
+// connection.once('open', () => {
+//   console.log("MongoDB database connection established successfully");
+// })
+
+// Routes to all the APIs
+//const userRouter = require('./api/profileManagement');
+//app.use('/user', userRouter);
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
