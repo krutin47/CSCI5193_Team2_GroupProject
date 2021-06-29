8ffdeac5ceab8891f63475e3d669f426414ebaf7
krutin47 <krutin47trivedi@gmail.com>
2021-06-28 22:20:00 -0300

    Corrected the email and password with enviromental variable. chnged the originala password of account.

--- utils/util.email.js
+++ utils/util.email.js
@@ -11,13 +11,13 @@ var sendMailMethods = {
         var transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: 'svachcart@gmail.com',
-                pass: 'Admin@123'
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD
             }
         });
         
         var mailOptions = {
-            from: 'svachcart@gmail.com',
+            from: process.env.EMAIL,
             to: email,
             subject: 'Svachcart',
             html: '<h1>Forgot your password</h1>' +
@@ -41,13 +41,13 @@ var sendMailMethods = {
 		var transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
-                user: 'svachcart@gmail.com',
-                pass: 'Admin@123'
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD
             }
         });
         
         var mailOptions = {
-            from: 'svachcart@gmail.com',
+            from: process.env.EMAIL,
             to: email,
             subject: 'Svachcart',
             html: require('./templates/index.js').orderConfirmation(),
@@ -72,13 +72,6 @@ var sendMailMethods = {
                 path: './utils/templates/images/image-6.png',
                 cid: 'delivery@nodemailer.com' //same cid value as in the html img src
             },]
-            
-            // '<h1>Hello there ' + firstName + " " + lastName + '..!</h1>' +
-            //             '<p>How are you doing..?</p>' +
-            //             '<br/>' +
-            //             '<p>our team will get back to you shortly. please write any query you have in this mail thread.</p>' +
-            //             '<br/>' +
-            //             '<p>until then, Have good day ' + firstName + " " + lastName + '..! will see you soon</p>'
         };
         
         transporter.sendMail(mailOptions, function(error, info){
